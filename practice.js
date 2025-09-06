16c8bea55a204a4368d85191216af428ec004437
Giriprasad1432 <giribabu121989@gmail.com>
2025-09-06 12:35:16 +0530

    js file modified

--- practice.js
+++ practice.js
@@ -1,3 +1,14 @@
-
-
-console.log("Hello world")
+let a=20
+for (i=0;i<a;i++){
+    console.log(i)
+}
+console.log(typeof(a))
+name = "giri"
+console.log(typeof(b))
+let ar=[1,2,3,4,5]
+console.log(typeof(ar))
+let person={
+ name:"giri",
+ age:"18"
+}
+console.log(person.name)
