e2060965a493b5d2761108d8c8b2047f30ff81cf
Giriprasad1432 <giribabu121989@gmail.com>
2025-09-06 12:38:19 +0530

    js file modified

--- practice.js
+++ practice.js
@@ -5,8 +5,10 @@ for (i=0;i<a;i++){
 console.log(typeof(a))
 name = "giri"
 console.log(typeof(b))
-let ar=[1,2,3,4,5]
+let ar=[1,2,3,4,5];
 console.log(typeof(ar))
+console.log(ar[0])
+console.log(ar[1])
 let person={
  name:"giri",
  age:"18"
